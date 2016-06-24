04c666b7e8170ca86255e5c55ab515f6d024e760
Nick Spinale <spinalen@carleton.edu>
2016-06-24 19:24:08 -0400

    Use ansi-terminal

--- src/Data/LOT.hs
+++ src/Data/LOT.hs
@@ -1,20 +1,37 @@
 module Data.LOT
-    ( LOT
+    ( render
+    , leftpad
+    , rightpad
+    , printables
+    , Chunk(..)
     ) where
 
 import Data.Text (Text)
+import System.Console.ANSI
+import Control.Applicative
 import qualified Data.Text as T
 
-data LOT = LOT Side Piece
-data Piece = Raw String | Stack Int [LOT]
+-- data These a b = This a | That b | These a b
 
-data Side = L | R
+-- data CenterStrategy = Absolute | Relative
 
--- data LOT = Raw String
---          | Left LOT LOT
---          | Right LOT LOT
---          | Center LOT LOT LOT
+-- data LOT = Center CenterStrategy [Fixed] (Maybe [Fixed]) (Maybe [Fixed])
+--          | NoCenter (These [Fixed] [Fixed])
 
--- renderWidth :: Int -> LOT -> Maybe String
--- renderWidth w (Raw str) | length str > w = Nothing
+-- data Fixed = Raw Text
+--            | Box Int LOT
+
+data Chunk = Chunk { sgr :: [SGR]
+                   , contents :: String
+                   }
+
+printables :: [Chunk] -> Int
+printables = sum . map (length . contents)
+
+render :: [Chunk] -> String
+render = concatMap $ liftA2 (\a b -> setSGRCode [] ++ a ++ b) (setSGRCode . sgr) contents
+
+leftpad, rightpad :: Int -> String -> String
+leftpad  n s =      replicate (max 0 (n - length s)) ' ' ++ s
+rightpad n s = s ++ replicate (max 0 (n - length s)) ' '
 
